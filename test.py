ab9fb5849a43a856b1dc3c2c693042435e736b6b
ygahabcd <136472595+ygahabcd@users.noreply.github.com>
2023-07-01 14:35:37 +0800

    updated to HOG

--- test.py
+++ test.py
@@ -1,12 +1,2 @@
-def square(x):
-    return x * x
-
-def successor(k):
-    return k + 1
-
-def compose1(f, g):
-        def h(x):
-            return f(g(x))
-        return h
-add_one_and_square = compose1(square, successor)
-print(add_one_and_square(12))
+for i in range(7):
+    print(type(i))
